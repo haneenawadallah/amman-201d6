a03db1c5247a39d84aab9156371f4e1fff2030cc
haneenawadallah <haneen.awadallah.995@gmail.com>
2020-06-13 18:52:13 +0300

    q4 is solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -82,13 +82,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) { 
+    
+var x = sum(sumArr[0] , sumArr[1])[0];
+var q4 = new Array(sum( x , sumArr[2])[0] , "2,3,4 was passed in as an array of numbers, and 9 is their sum." );
+
+return q4;
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
